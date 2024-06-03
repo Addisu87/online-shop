fda27ce11c4be88d32790489b4c27c858c9b3b4d
Addisu <addisuhaile87@gmail.com>
2024-06-03 10:55:50 +0300

    Create shop and cart apps

---
+++ cart/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+
+
+class CartAddProductForm(forms.Form):
+    # Adding items to the cart
+    # this allows the user to select a quantity between 1 and 20
+    quantity = forms.TypedChoiceField(
+        choices=PRODUCT_QUANTITY_CHOICES,
+        coerce=int
+    )
+   #  to indicate whether the quantity has to be added or not
+    override = forms.BooleanField(
+        required=False,
+        initial=False,
+        widget=forms.HiddenInput
+    )
