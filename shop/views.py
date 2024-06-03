2a1f4c3d7b45b3904e2d9c3657b0d7039ca0f54a
Addisu <addisuhaile87@gmail.com>
2024-06-03 16:32:22 +0300

    Update cart details

--- shop/views.py
+++ shop/views.py
@@ -10,10 +10,10 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html',
-                  {'category': category, 'categories': categories,
-                   'products': products
-                   })
+    return render(
+        request, 'shop/product/list.html', {'category': category,
+                                            'categories': categories, 'products': products
+                                            })
 
 
 def product_detail(request, id, slug):
@@ -23,5 +23,5 @@ def product_detail(request, id, slug):
     cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html', {'product': product,
-                                               'cart_product_form': cart_product_form}
-                  )
+                                               'cart_product_form': cart_product_form
+                                               })
